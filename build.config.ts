d5e31896299474fca2c4a359b41699f4c4231782
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-15 11:57:57 +0300

    🤏 create minified build

--- build.config.ts
+++ build.config.ts
@@ -1,3 +1,13 @@
-export default {
+// @ts-ignore
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+	name: 'minified',
 	entries: ['code.ts'],
-};
+	outDir: 'dist',
+	rollup: {
+		esbuild: {
+			minify: true,
+		},
+	},
+});

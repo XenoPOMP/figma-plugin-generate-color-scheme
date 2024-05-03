f126d269ea7c04f68be3db2daa0bf87b7b72918e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-03 11:25:55 +0300

    🤏 move all text creation logic to func

--- code.ts
+++ code.ts
@@ -8,29 +8,9 @@ import { createText, getPaintTree } from './src/lib/utils';
 		.trim();
 
 	/** Create figma text node. */
-	const outputText = createText('Generated TW config code');
+	await createText('Generated TW config code', textToDisplay);
 
-	figma
-		.loadFontAsync(outputText.fontName as FontName)
-		.then(() => {
-			outputText.characters = textToDisplay;
-
-			outputText.fontSize = 50;
-			outputText.textAutoResize = 'HEIGHT';
-			outputText.fills = [
-				{
-					type: 'SOLID',
-					color: {
-						r: 1,
-						g: 1,
-						b: 1,
-					},
-					opacity: 1,
-				},
-			];
-		})
-		.finally(() => {
-			figma.notify('Generated TW config for you.');
-			figma.closePlugin();
-		});
+	/** Successful plugin execution. */
+	figma.notify('Generated TW config for you.');
+	figma.closePlugin();
 })();
--- src/lib/utils/create-text.ts
+++ src/lib/utils/create-text.ts
@@ -1,15 +1,58 @@
+type NumericRecord<TType extends string | number | symbol> = Record<
+	TType,
+	number
+>;
+
+interface ICreateTextOptions
+	extends NumericRecord<'x' | 'y' | 'width' | 'height'> {}
+
 /**
  * Creates figma text and returns it.
  * @param name
+ * @param content
+ * @param options
  */
-export const createText = (name: string) => {
+export const createText = async (
+	name: string,
+	content: string,
+	options: ICreateTextOptions = {
+		x: -4800,
+		y: -1472,
+		width: 2549,
+		height: 3203,
+	}
+) => {
+	/** Created Figma`s text node. */
 	const text = figma.createText();
 
+	/** Change metadata of text. */
 	text.name = name;
-	text.x = -4800;
-	text.y = -1472;
+	text.x = options.x;
+	text.y = options.y;
 
-	text.resizeWithoutConstraints(2549, 3203);
+	/** Change sizes of text */
+	text.resizeWithoutConstraints(options.width, options.height);
+
+	/** Load font asynchronously. */
+	await figma.loadFontAsync(text.fontName as FontName);
+
+	/** Set content of text node. */
+	text.characters = content;
+
+	/** Change styles of text. */
+	text.fontSize = 50;
+	text.textAutoResize = 'HEIGHT';
+	text.fills = [
+		{
+			type: 'SOLID',
+			color: {
+				r: 1,
+				g: 1,
+				b: 1,
+			},
+			opacity: 1,
+		},
+	];
 
 	return text;
 };
